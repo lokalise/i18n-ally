ac42e8230edd39a273312d4ee3c54696cd9b0a04
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-03 20:32:04 +0800

    refactor: extension registration

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,27 @@
-import { Common, LocaleLoader } from './core'
 import { ExtensionContext, workspace, commands } from 'vscode'
+import { flatten } from 'lodash'
+import { Common, LocaleLoader } from './core'
+import commandsModules from './commands'
+import editorModules from './editor'
+import viewsModules from './views'
 
 export async function activate (ctx: ExtensionContext) {
-  if (!workspace.workspaceFolders || !(await Common.isVueProject())) {
-    console.log('vue-i18n is inactive')
+  // Deactivate for non vue-i18n enabled project
+  if (!workspace.workspaceFolders || !(await Common.isVueProject()))
     return
-  }
 
+  // activate the extension
   Common.loader = new LocaleLoader()
   await Common.loader.init()
   commands.executeCommand('setContext', 'vue-i18n-ally-enabled', true)
 
-  console.log('Vue-i18n is active')
-  ;[
-    require('./commands').default,
-    require('./editor').default,
-    require('./views').default,
-  ].forEach(module => ctx.subscriptions.push(module(ctx)))
+  const modules = [
+    commandsModules,
+    editorModules,
+    viewsModules,
+  ]
+  const disposables = flatten(modules.map(m => m(ctx)))
+  disposables.forEach(d => ctx.subscriptions.push(d))
 }
 
 export function deactivate () {}
