bfd3c83d0e3db7fa9d5e384eb5b523c8cb68ec1a
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-03 10:35:41 +0800

    feat: edit keys in sidebar

--- src/views/LocalesTreeView.ts
+++ src/views/LocalesTreeView.ts
@@ -1,11 +1,11 @@
-import * as vscode from 'vscode'
 import * as clipboardy from 'clipboardy'
 import { Common, LocaleLoader, LocaleNode, LocaleRecord, LocaleTree } from '../core'
 import { ExtensionModule } from '../modules'
+import { TreeItem, ExtensionContext, TreeItemCollapsibleState, TreeDataProvider, EventEmitter, Event, window, commands } from 'vscode'
 
-export class Item extends vscode.TreeItem {
+export class Item extends TreeItem {
   constructor (
-    private ctx: vscode.ExtensionContext,
+    private ctx: ExtensionContext,
     public readonly node: LocaleNode | LocaleRecord | LocaleTree
   ) {
     super('')
@@ -26,9 +26,9 @@ export class Item extends vscode.TreeItem {
 
   get collapsibleState () {
     if (this.node.type === 'record')
-      return vscode.TreeItemCollapsibleState.None
+      return TreeItemCollapsibleState.None
     else
-      return vscode.TreeItemCollapsibleState.Collapsed
+      return TreeItemCollapsibleState.Collapsed
   }
 
   set collapsibleState (_) {}
@@ -54,13 +54,13 @@ export class Item extends vscode.TreeItem {
   }
 }
 
-export class LocalesTreeProvider implements vscode.TreeDataProvider<Item> {
-  private _onDidChangeTreeData: vscode.EventEmitter<Item | undefined> = new vscode.EventEmitter<Item | undefined>();
-  readonly onDidChangeTreeData: vscode.Event<Item | undefined> = this._onDidChangeTreeData.event;
+export class LocalesTreeProvider implements TreeDataProvider<Item> {
+  private _onDidChangeTreeData: EventEmitter<Item | undefined> = new EventEmitter<Item | undefined>();
+  readonly onDidChangeTreeData: Event<Item | undefined> = this._onDidChangeTreeData.event;
   private loader: LocaleLoader
-  private ctx: vscode.ExtensionContext
+  private ctx: ExtensionContext
 
-  constructor (ctx: vscode.ExtensionContext) {
+  constructor (ctx: ExtensionContext) {
     this.ctx = ctx
     this.loader = Common.loader
     this.loader.addEventListener('changed', () => this.refresh())
@@ -70,7 +70,7 @@ export class LocalesTreeProvider implements vscode.TreeDataProvider<Item> {
     this._onDidChangeTreeData.fire()
   }
 
-  getTreeItem (element: Item): vscode.TreeItem {
+  getTreeItem (element: Item): TreeItem {
     return element
   }
 
@@ -95,22 +95,43 @@ export class LocalesTreeProvider implements vscode.TreeDataProvider<Item> {
 
 const m: ExtensionModule = (ctx) => {
   const provider = new LocalesTreeProvider(ctx)
-  vscode.window.registerTreeDataProvider('locales-tree', provider)
-  vscode.commands.registerCommand('extension.vue-i18n-ally.copy-key', ({ node }: {node: LocaleNode}) => {
+  window.registerTreeDataProvider('locales-tree', provider)
+  commands.registerCommand('extension.vue-i18n-ally.copy-key', ({ node }: {node: LocaleNode}) => {
     clipboardy.writeSync(`$t('${node.keypath}')`)
-    vscode.window.showInformationMessage('I18n key copied')
+    window.showInformationMessage('I18n key copied')
   })
 
-  vscode.commands.registerCommand('extension.vue-i18n-ally.translate-key', async ({ node }: {node: LocaleRecord}) => {
+  commands.registerCommand('extension.vue-i18n-ally.translate-key', async ({ node }: {node: LocaleRecord}) => {
     try {
       const pending = await Common.loader.MachineTranslateRecord(node)
       if (pending) {
         await Common.loader.writeToFile(pending)
-        vscode.window.showInformationMessage('Translation saved!')
+        window.showInformationMessage('Translation saved!')
       }
     }
     catch (err) {
-      vscode.window.showErrorMessage(err.toString())
+      window.showErrorMessage(err.toString())
+    }
+  })
+
+  commands.registerCommand('extension.vue-i18n-ally.edit-key', async ({ node }: {node: LocaleRecord}) => {
+    try {
+      const newvalue = await window.showInputBox({
+        value: node.value,
+        prompt: `Edit key "${node.keypath}" on ${node.locale}`,
+      })
+
+      if (newvalue !== undefined && newvalue !== node.value) {
+        await Common.loader.writeToFile({
+          value: newvalue,
+          keypath: node.keypath,
+          filepath: node.filepath,
+          locale: node.locale,
+        })
+      }
+    }
+    catch (err) {
+      window.showErrorMessage(err.toString())
     }
   })
 }
