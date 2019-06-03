1f8df72b2d1667f9df5e5aef4bf105e3f3156587
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-03 11:56:53 +0800

    refactor: locale tree view
    refactor

--- src/views/LocalesTreeView.ts
+++ src/views/LocalesTreeView.ts
@@ -3,10 +3,13 @@ import { Common, LocaleLoader, LocaleNode, LocaleRecord, LocaleTree } from '../c
 import { ExtensionModule } from '../modules'
 import { TreeItem, ExtensionContext, TreeItemCollapsibleState, TreeDataProvider, EventEmitter, Event, window, commands } from 'vscode'
 
+export type Node = LocaleNode | LocaleRecord | LocaleTree
+
 export class Item extends TreeItem {
   constructor (
     private ctx: ExtensionContext,
-    public readonly node: LocaleNode | LocaleRecord | LocaleTree
+    public readonly node: Node,
+    public flatten = false
   ) {
     super('')
   }
@@ -16,10 +19,14 @@ export class Item extends TreeItem {
   }
 
   get label (): string {
-    if (this.node.type === 'record')
+    if (this.node.type === 'record') {
       return this.node.locale
-    else
-      return this.node.keyname
+    }
+    else {
+      return this.flatten
+        ? this.node.keypath
+        : this.node.keyname
+    }
   }
 
   set label (_) {}
@@ -55,18 +62,22 @@ export class Item extends TreeItem {
 }
 
 export class LocalesTreeProvider implements TreeDataProvider<Item> {
+  private loader: LocaleLoader
+  private _flatten: boolean
   private _onDidChangeTreeData: EventEmitter<Item | undefined> = new EventEmitter<Item | undefined>();
   readonly onDidChangeTreeData: Event<Item | undefined> = this._onDidChangeTreeData.event;
-  private loader: LocaleLoader
-  private ctx: ExtensionContext
 
-  constructor (ctx: ExtensionContext) {
-    this.ctx = ctx
+  constructor (
+    private ctx: ExtensionContext,
+    public includePaths?: string[],
+    flatten = false,
+  ) {
+    this._flatten = flatten
     this.loader = Common.loader
     this.loader.addEventListener('changed', () => this.refresh())
   }
 
-  refresh (): void {
+  private refresh (): void {
     this._onDidChangeTreeData.fire()
   }
 
@@ -74,22 +85,57 @@ export class LocalesTreeProvider implements TreeDataProvider<Item> {
     return element
   }
 
+  newItem (node: Node) {
+    return new Item(this.ctx, node, this.flatten)
+  }
+
+  get flatten () {
+    return this._flatten
+  }
+
+  set flatten (value) {
+    if (this._flatten !== value) {
+      this._flatten = value
+      this.refresh()
+    }
+  }
+
+  private filter (node: Node): boolean {
+    if (!this.includePaths)
+      return true
+
+    for (const includePath of this.includePaths) {
+      if (includePath.startsWith(node.keypath))
+        return true
+    }
+    return false
+  }
+
+  private getRoots () {
+    const nodes = this.flatten
+      ? Object.values(this.loader.flattenLocaleTree)
+      : Object.values(this.loader.localeTree.children)
+
+    return nodes
+      .filter(node => this.filter(node))
+      .map(node => this.newItem(node))
+  }
+
   async getChildren (element?: Item) {
-    if (element) {
-      if (element.node.type === 'tree') {
-        return Object.values(element.node.children)
-          .map(r => new Item(this.ctx, r))
-      }
-      if (element.node.type === 'node') {
-        return Object.values(this.loader.getShadowLocales(element.node))
-          .map(r => new Item(this.ctx, r))
-      }
-      return []
-    }
-    else {
-      return Object.values(this.loader.localeTree.children)
-        .map(node => new Item(this.ctx, node))
-    }
+    if (!element)
+      return this.getRoots()
+
+    let nodes: Node[] = []
+
+    if (element.node.type === 'tree')
+      nodes = Object.values(element.node.children)
+
+    else if (element.node.type === 'node')
+      nodes = Object.values(this.loader.getShadowLocales(element.node))
+
+    return nodes
+      .filter(node => this.filter(node))
+      .map(r => this.newItem(r))
   }
 }
 
@@ -102,45 +148,49 @@ export class LocalesTreeView {
 
 const m: ExtensionModule = (ctx) => {
   const provider = new LocalesTreeProvider(ctx)
-  window.registerTreeDataProvider('locales-tree', provider)
-  commands.registerCommand('extension.vue-i18n-ally.copy-key', ({ node }: {node: LocaleNode}) => {
-    clipboardy.writeSync(`$t('${node.keypath}')`)
-    window.showInformationMessage('I18n key copied')
-  })
 
-  commands.registerCommand('extension.vue-i18n-ally.translate-key', async ({ node }: {node: LocaleRecord}) => {
-    try {
-      const pending = await Common.loader.MachineTranslateRecord(node)
-      if (pending) {
-        await Common.loader.writeToFile(pending)
-        window.showInformationMessage('Translation saved!')
+  return [
+    window.registerTreeDataProvider('locales-tree', provider),
+
+    commands.registerCommand('extension.vue-i18n-ally.copy-key', ({ node }: {node: LocaleNode}) => {
+      clipboardy.writeSync(`$t('${node.keypath}')`)
+      window.showInformationMessage('I18n key copied')
+    }),
+
+    commands.registerCommand('extension.vue-i18n-ally.translate-key', async ({ node }: {node: LocaleRecord}) => {
+      try {
+        const pending = await Common.loader.MachineTranslateRecord(node)
+        if (pending) {
+          await Common.loader.writeToFile(pending)
+          window.showInformationMessage('Translation saved!')
+        }
       }
-    }
-    catch (err) {
-      window.showErrorMessage(err.toString())
-    }
-  })
+      catch (err) {
+        window.showErrorMessage(err.toString())
+      }
+    }),
 
-  commands.registerCommand('extension.vue-i18n-ally.edit-key', async ({ node }: {node: LocaleRecord}) => {
-    try {
-      const newvalue = await window.showInputBox({
-        value: node.value,
-        prompt: `Edit key "${node.keypath}" on ${node.locale}`,
-      })
-
-      if (newvalue !== undefined && newvalue !== node.value) {
-        await Common.loader.writeToFile({
-          value: newvalue,
-          keypath: node.keypath,
-          filepath: node.filepath,
-          locale: node.locale,
+    commands.registerCommand('extension.vue-i18n-ally.edit-key', async ({ node }: {node: LocaleRecord}) => {
+      try {
+        const newvalue = await window.showInputBox({
+          value: node.value,
+          prompt: `Edit key "${node.keypath}" on ${node.locale}`,
         })
+
+        if (newvalue !== undefined && newvalue !== node.value) {
+          await Common.loader.writeToFile({
+            value: newvalue,
+            keypath: node.keypath,
+            filepath: node.filepath,
+            locale: node.locale,
+          })
+        }
       }
-    }
-    catch (err) {
-      window.showErrorMessage(err.toString())
-    }
-  })
+      catch (err) {
+        window.showErrorMessage(err.toString())
+      }
+    }),
+  ]
 }
 
 export default m
