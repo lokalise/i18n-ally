3304e977051055305f91532dd354d25c8fbddaa6
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-05 13:19:20 +0800

    chore: add demo fixture

---
+++ test/fixture/demo/Demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>{{ $t('hello') }}</p>
+
+    <p v-t="'nested.hello'"></p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    foobar() {
+      return this.$t('nested.foo.bar')
+    },
+  },
+}
+</script>
