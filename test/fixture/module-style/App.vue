44e8c57a6c3cd24e988cef159ce88e4c7727bd5e
Anthony Fu <anthonyfu117@hotmail.com>
2019-07-09 21:14:26 +0800

    docs: add module-style fixture

---
+++ test/fixture/module-style/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <p>{{ $t('one') }}</p>
+    <p>{{ $t('two') }}</p>
+    <p>{{ $t('three') }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
