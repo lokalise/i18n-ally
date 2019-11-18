4da33f41ba4df31f92ec0f8c834e910cec8d20d7
Anthony Fu <anthonyfu117@hotmail.com>
2019-11-18 11:42:29 +0800

    refactor(sfc): integrate with `vue-i18n-locale-message`

--- src/core/loaders/SfcLoader.ts
+++ src/core/loaders/SfcLoader.ts
@@ -1,22 +1,9 @@
-import { extname } from 'path'
 import { workspace, Uri } from 'vscode'
-import { parse, HTMLElement } from 'node-html-parser'
+import { squeeze, SFCI18nBlock } from 'vue-i18n-locale-message'
 import { Log } from '../../utils'
 import { LocaleTree } from '../types'
-import { Global } from '../Global'
 import { Loader, NodeOptions } from './Loader'
-
-interface Section {
-  lang: string
-  index: number
-  locale?: string
-  content?: string
-  src?: string
-}
-
-interface ParseredSection extends Section{
-  data: any
-}
+import { Global } from '..'
 
 export class SFCLoader extends Loader {
   constructor (
@@ -31,58 +18,23 @@ export class SFCLoader extends Loader {
     return [{ filepath: this.uri.fsPath, locale: '', nested: false }]
   }
 
-  private getSections (text: string): Section[] {
-    const root = parse(text)
-    const elements = root.childNodes.filter(node => node instanceof HTMLElement && node.tagName === 'i18n') as HTMLElement[]
-    return elements.map((el, index) => {
-      const query = el.attributes
-      const lang = query.lang || extname(query.src || '') || 'json'
-      return {
-        ...query,
-        index,
-        lang,
-        content: el.innerHTML,
-      }
-    })
-  }
-
-  private async loadSection (section: Section) {
-    if (!section.content && !section.src)
-      return undefined
-
-    if (section.content && section.src)
-      throw new Error('can not use inline and "src" at the same time')
-
-    const parser = Global.getMatchedParser(section.lang)
-
-    if (!parser)
-      throw new Error(`unsupported lang ${section.lang}`)
-
-    let data: any
-    if (section.src)
-      data = await parser.load(section.src)
-    else if (section.content)
-      data = await parser.parse(section.content)
-
-    return data
-  }
-
-  _parsedSections: ParseredSection[] = []
+  _parsedSections: SFCI18nBlock[] = []
 
   async load () {
-    Log.info(`📑 Loading sfc ${this.uri.fsPath}`)
-    this._parsedSections = []
+    const filepath = this.uri.fsPath
+    Log.info(`📑 Loading sfc ${filepath}`)
     const doc = await workspace.openTextDocument(this.uri)
-    const sections = this.getSections(doc.getText())
-    for (const section of sections) {
-      const data = await this.loadSection(section)
-      this._parsedSections.push({ ...section, data })
-    }
+    const meta = squeeze(Global.rootpath, [{
+      path: filepath,
+      content: doc.getText(),
+    }])
+    this._parsedSections = meta.components[filepath]
+
     this.updateLocalesTree()
     this._onDidChange.fire(this.name)
   }
 
-  private getOptions (section: ParseredSection, locale: string): NodeOptions {
+  private getOptions (section: SFCI18nBlock, locale: string): NodeOptions {
     return {
       filepath: section.src || this.uri.fsPath,
       locale,
@@ -99,17 +51,11 @@ export class SFCLoader extends Loader {
 
     const tree = new LocaleTree({ keypath: '', sfc: true })
     for (const section of this._parsedSections) {
-      if (!section.data)
+      if (!section.messages)
         continue
-      if (section.locale) {
-        this._locales.add(section.locale)
-        this.updateTree(tree, section.data, '', '', this.getOptions(section, section.locale))
-      }
-      else {
-        for (const [locale, value] of Object.entries(section.data)) {
-          this._locales.add(locale)
-          this.updateTree(tree, value, '', '', this.getOptions(section, locale))
-        }
+      for (const [locale, value] of Object.entries(section.messages)) {
+        this._locales.add(locale)
+        this.updateTree(tree, value, '', '', this.getOptions(section, locale))
       }
     }
     this._localeTree = tree
