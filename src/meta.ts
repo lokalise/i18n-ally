e9dc4813cf6f34a3a95197cf93803c9cb437c5da
Anthony Fu <anthonyfu117@hotmail.com>
2019-11-20 02:29:43 +0800

    chore: rename extension id

--- src/meta.ts
+++ src/meta.ts
@@ -1,6 +1,7 @@
 export const EXT_NAMESPACE = 'i18n-ally'
 export const EXT_LEGACY_NAMESPACE = 'vue-i18n-ally'
-export const EXT_ID = 'antfu.vue-i18n-ally'
+export const EXT_ID = 'antfu.i18n-ally'
+export const EXT_LEGACY_ID = 'antfu.vue-i18n-ally'
 export const EXT_NAME = 'i18n Ally'
 
 // TODO: make following values dynamic
