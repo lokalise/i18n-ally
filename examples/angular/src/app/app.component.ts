dda136c62c816bd192cd02a70f18939f5819acf3
Anthony Fu <anthonyfu117@hotmail.com>
2019-11-20 01:41:33 +0800

    docs: add angular example

---
+++ examples/angular/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, LOCALE_ID, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'i18nDemo';
+
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'hi', label: 'हिंदी' },
+    { code: 'es', label: 'Espanol' }
+
+  ];
+
+  constructor(@Inject(LOCALE_ID) protected localeId: string) { }
+}
