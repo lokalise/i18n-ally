15bd197d329996f4c1ebe7d128004d55f3c44f1a
Anthony Fu <anthonyfu117@hotmail.com>
2019-11-21 14:08:42 +0800

    ui: new logo

--- src/meta.ts
+++ src/meta.ts
@@ -6,5 +6,3 @@ export const EXT_NAME = 'i18n Ally'
 
 // TODO: make following values dynamic
 export const SUPPORTED_LANG_GLOBS = '**/*.{js,jsx,ts,tsx,mjs,vue}'
-export const MATCH_REG_FILE = '^([\\w-_]*)\\.(json5?|ya?ml|jsx?|tsx?|mjs)$'
-export const MATCH_REG_DIR = '^(.*)\\.(json5?|ya?ml|jsx?|tsx?|mjs)$'
