2307fa5bbc8e88934a467187c236414ce625d53c
Anthony Fu <anthonyfu117@hotmail.com>
2019-11-21 16:56:31 +0800

    chore: add beta link

--- src/extension.ts
+++ src/extension.ts
@@ -7,25 +7,25 @@ import editorModules from './editor'
 import viewsModules from './views'
 import { Log } from './utils'
 import { CurrentFile } from './core/CurrentFile'
-import { EXT_ID_I18N_ALLY } from './meta.js'
+import { EXT_ID_I18N_ALLY, I18N_ALLY_URL } from './meta.js'
 import i18n from './i18n.js'
 
 export async function checkI18nAllyExists () {
   const extension = extensions.getExtension(EXT_ID_I18N_ALLY)
   if (extension) {
-    Log.info('🚧' + i18n.t('prompt.disabled_due_to_new_version'))
+    Log.info(`🚧${i18n.t('prompt.disabled_due_to_new_version')}`)
     window.showWarningMessage(i18n.t('prompt.disabled_due_to_new_version'))
   }
   else {
-    Log.info('✨' + i18n.t('prompt.i18n_ally_beta_avaliable'))
+    Log.info(`✨${i18n.t('prompt.i18n_ally_beta_avaliable')}`)
+    Log.info(I18N_ALLY_URL)
   }
   return !!extension
 }
 
 export async function activate (ctx: ExtensionContext) {
-  if (await checkI18nAllyExists()){
+  if (await checkI18nAllyExists())
     return
-  }
 
   Log.info(`🈶 Activated, v${version}`)
 
--- src/meta.ts
+++ src/meta.ts
@@ -3,6 +3,8 @@ export const EXT_ID = 'antfu.vue-i18n-ally'
 export const EXT_ID_I18N_ALLY = 'antfu.i18n-ally'
 export const EXT_NAME = 'Vue i18n Ally'
 
+export const I18N_ALLY_URL = 'https://marketplace.visualstudio.com/items?itemName=antfu.i18n-ally'
+
 // TODO: make following values dynamic
 export const SUPPORTED_LANG_GLOBS = '**/*.{js,jsx,ts,tsx,mjs,vue}'
 export const MATCH_REG_FILE = '^([\\w-_]*)\\.(json5?|ya?ml|jsx?|tsx?|mjs)$'
