3a8489f0d2970ae313631058d1ad7d29a13c1be2
Anthony Fu <anthonyfu117@hotmail.com>
2019-12-21 04:22:22 +0800

    fix: copying key should not come with $t, #151 thanks @JohanBeekers

--- src/commands/manipulations/copyKey.ts
+++ src/commands/manipulations/copyKey.ts
@@ -3,7 +3,6 @@ import { LocaleTreeItem } from '../../views'
 import i18n from '../../i18n'
 
 export async function CopyKey ({ node }: LocaleTreeItem) {
-  // @ts-ignore
-  await env.clipboard.writeText(`$t('${node.keypath}')`)
+  await env.clipboard.writeText(node.keypath)
   window.showInformationMessage(i18n.t('prompt.key_copied'))
 }
