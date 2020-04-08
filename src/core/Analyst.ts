3c9e6aa2b1cab7b053f9eb40b53ba7cc136d7b4a
Anthony Fu <anthonyfu117@hotmail.com>
2020-04-08 21:11:07 +0800

    i18n: remove unused keys

--- src/core/Analyst.ts
+++ src/core/Analyst.ts
@@ -1,17 +1,16 @@
 import { resolve, join } from 'path'
 import fs from 'fs'
 import { workspace, Range, Location, TextDocument, Uri, EventEmitter } from 'vscode'
-
-import { Log } from '../utils'
-import { Global } from './Global'
-import { CurrentFile } from './CurrentFile'
-import { UsageReport } from './types'
-import { KeyDetector, Config, KeyOccurrence, KeyUsage } from '.'
 // @ts-ignore
 import { glob } from 'glob-gitignore'
 // @ts-ignore
 import parseGitIgnore from 'parse-gitignore'
 import _, { uniq } from 'lodash'
+import { Log } from '../utils'
+import { Global } from './Global'
+import { CurrentFile } from './CurrentFile'
+import { UsageReport } from './types'
+import { KeyDetector, Config, KeyOccurrence, KeyUsage } from '.'
 
 export class Analyst {
   private static _cache: KeyOccurrence[] | null = null
