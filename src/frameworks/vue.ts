267ca5acdc16010ca09d024c52ac55980de1d07e
Anthony Fu <anthonyfu117@hotmail.com>
2020-04-21 02:41:19 +0800

    fix(vue): remove $d and $n support, resolves #254

--- src/frameworks/vue.ts
+++ src/frameworks/vue.ts
@@ -24,7 +24,7 @@ class VueFramework extends Framework {
 
   // for visualize the regex, you can use https://regexper.com/
   usageMatchRegex = [
-    '(?:i18n[ (]path=|v-t=[\'"`{]|(?:this\\.|\\$|i18n\\.)(?:(?:d|n)\\(.*?,|(?:t|tc|te)\\()\\s*)[\'"`]({key})[\'"`]',
+    '(?:i18n[ (]path=|v-t=[\'"`{]|(?:this\\.|\\$|i18n\\.)(?:t|tc|te)\\()\\s*[\'"`]({key})[\'"`]',
   ]
 
   refactorTemplates(keypath: string, languageId: string) {
