a479d2529b87f796f3b87aa55df516ef07ebb09a
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-14 20:55:40 +0800

    fix: close #350

--- src/frameworks/ngx-translate.ts
+++ src/frameworks/ngx-translate.ts
@@ -20,7 +20,7 @@ class NgxTranslateFramework extends Framework {
   // for visualize the regex, you can use https://regexper.com/
   usageMatchRegex= [
     '[`\'"]({key})[`\'"][\\s\\n]*\\|[\\s\\n]*translate',
-    'translate\\.get\\([\\s\\n]*[\'"`]({key})[\'"`]',
+    '(?:translate|translateService)\\.(?:get|instant)\\([\\s\\n]*[\'"`]({key})[\'"`]',
     '[\\s\\n]translate>[\\s\\n]*({key})[\\s\\n]*</',
   ]
 
