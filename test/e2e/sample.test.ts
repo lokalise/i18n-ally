2f00c9e6014e1dd2717b3fc516ced4989e119845
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-29 00:38:40 +0800

    test: refactor

--- test/e2e/sample.test.ts
+++ test/e2e/sample.test.ts
@@ -1,34 +1,16 @@
 // import the webdriver and the high level browser wrapper
-import { resolve } from 'path'
 import { expect } from 'chai'
-import { VSBrowser, WebDriver, Workbench, InputBox, until } from 'vscode-extension-tester'
+import { getContext, openFixture } from './utils'
 
 describe('sample', () => {
-  let browser: VSBrowser
-  let driver: WebDriver
-
-  before(async() => {
-    browser = VSBrowser.instance
-    driver = browser.driver
-  })
-
   it('welcome', async() => {
+    const { driver } = getContext()
     const title = await driver.getTitle()
     expect(title.startsWith('Welcome')).to.eql(true)
   })
 
   it('open fixture', async() => {
-    const workbench = new Workbench()
-    await workbench.executeCommand('workbench.action.files.openFileFolder')
-    await driver.sleep(100)
-    const input = await InputBox.create()
-    await input.setText(`${resolve(__dirname, '../../examples/by-frameworks/vue-i18n')}/`)
-    await input.confirm()
-
-    // wait for workbench to unload
-    await driver.wait(until.stalenessOf(workbench))
-    await browser.waitForWorkbench()
-
+    const { driver } = await openFixture('examples/by-frameworks/vue-i18n')
     const title = await driver.getTitle()
     expect(title).to.eql('Welcome — vue-i18n')
   })
--- test/e2e/utils.ts
+++ test/e2e/utils.ts
@@ -1,3 +1,32 @@
+import { resolve } from 'path'
+import { VSBrowser, Workbench, InputBox, until } from 'vscode-extension-tester'
+
 export function timeout(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export function getContext() {
+  const workbench = new Workbench()
+  const browser = VSBrowser.instance
+  const driver = browser.driver
+
+  return { workbench, browser, driver }
+}
+
+export async function openFixture(path: string) {
+  const browser = VSBrowser.instance
+  const driver = browser.driver
+
+  const lastWorkbench = new Workbench()
+  await lastWorkbench.executeCommand('workbench.action.files.openFileFolder')
+  const input = await InputBox.create()
+  await input.setText(`${resolve(__dirname, '../../', path)}/`)
+  await input.confirm()
+
+  // wait for workbench to unload
+  await driver.wait(until.stalenessOf(lastWorkbench))
+  await browser.waitForWorkbench()
+
+  const workbench = new Workbench()
+  return { workbench, browser, driver }
 }
