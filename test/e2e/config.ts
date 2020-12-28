2a734181c0392f3dcbf7baaa432a0b9123530649
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-29 00:33:44 +0800

    test(e2e): open fixture

---
+++ test/e2e/config.ts
@@ -0,0 +1,8 @@
+import { MochaOptions } from 'vscode-extension-tester'
+
+const options: MochaOptions = {
+  timeout: 60_000,
+  slow: 5_000,
+}
+
+export default options
--- test/e2e/sample.test.ts
+++ test/e2e/sample.test.ts
@@ -1,21 +1,36 @@
 // import the webdriver and the high level browser wrapper
-import { VSBrowser, WebDriver } from 'vscode-extension-tester'
+import { resolve } from 'path'
 import { expect } from 'chai'
+import { VSBrowser, WebDriver, Workbench, InputBox, until } from 'vscode-extension-tester'
 
-// Create a Mocha suite
 describe('sample', () => {
   let browser: VSBrowser
   let driver: WebDriver
 
-  // initialize the browser and webdriver
   before(async() => {
     browser = VSBrowser.instance
     driver = browser.driver
   })
 
-  // test whatever we want using webdriver, here we are just checking the page title
-  it('sample 1', async() => {
+  it('welcome', async() => {
     const title = await driver.getTitle()
     expect(title.startsWith('Welcome')).to.eql(true)
   })
+
+  it('open fixture', async() => {
+    const workbench = new Workbench()
+    await workbench.executeCommand('workbench.action.files.openFileFolder')
+    await driver.sleep(100)
+    const input = await InputBox.create()
+    await input.setText(`${resolve(__dirname, '../../examples/by-frameworks/vue-i18n')}/`)
+    await input.confirm()
+
+    // wait for workbench to unload
+    await driver.wait(until.stalenessOf(workbench))
+    await browser.waitForWorkbench()
+
+    const title = await driver.getTitle()
+    expect(title).to.eql('Welcome — vue-i18n')
+  })
+    .timeout(60_000)
 })
