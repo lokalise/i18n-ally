23d94754131ae904a206d365ebf8b3ae65faeaac
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-11 18:04:16 +0800

    chore: commet out unused code

--- src/frameworks/laravel.ts
+++ src/frameworks/laravel.ts
@@ -1,6 +1,6 @@
-import { Range, TextDocument } from 'vscode'
-import Parser, { Node } from 'php-parser'
-import { Framework, HardStringInfo } from './base'
+// import { Range, TextDocument } from 'vscode'
+// import Parser, { Node } from 'php-parser'
+import { Framework } from './base'
 import { LanguageId } from '~/utils'
 
 class LaravelFramework extends Framework {
@@ -49,61 +49,61 @@ class LaravelFramework extends Framework {
   // TODO: not working yet
   // supportAutoExtraction = ['php']
 
-  _engine: Parser = undefined!
+  // _engine: Parser = undefined!
 
-  detectHardStrings(doc: TextDocument) {
-    if (doc.languageId !== 'php')
-      return undefined
+  // detectHardStrings(doc: TextDocument) {
+  //   if (doc.languageId !== 'php')
+  //     return undefined
 
-    const text = doc.getText()
+  //   const text = doc.getText()
 
-    const engine = this._engine = this._engine || new Parser({
-      parser: {
-        extractDoc: true,
-      },
-      ast: {
-        withPositions: true,
-      },
-    })
+  //   const engine = this._engine = this._engine || new Parser({
+  //     parser: {
+  //       extractDoc: true,
+  //     },
+  //     ast: {
+  //       withPositions: true,
+  //     },
+  //   })
 
-    const ast = engine.parseCode(text)
+  //   const ast = engine.parseCode(text)
 
-    // console.log('AST', ast)
+  //   // console.log('AST', ast)
 
-    function searchFor(name: string, node: any = ast): Node[] {
-      if (!node)
-        return []
-      if (node.kind === name)
-        return [node]
-      if (node.expression)
-        return searchFor(name, node.expression)
-      if (node.left || node.right) {
-        return [
-          ...searchFor(name, node.left),
-          ...searchFor(name, node.right),
-        ]
-      }
-      if (!node?.children?.length)
-        return []
-      return node.children.flatMap((i: any) => {
-        return searchFor(name, i)
-      })
-    }
+  //   function searchFor(name: string, node: any = ast): Node[] {
+  //     if (!node)
+  //       return []
+  //     if (node.kind === name)
+  //       return [node]
+  //     if (node.expression)
+  //       return searchFor(name, node.expression)
+  //     if (node.left || node.right) {
+  //       return [
+  //         ...searchFor(name, node.left),
+  //         ...searchFor(name, node.right),
+  //       ]
+  //     }
+  //     if (!node?.children?.length)
+  //       return []
+  //     return node.children.flatMap((i: any) => {
+  //       return searchFor(name, i)
+  //     })
+  //   }
 
-    const stringNodes = searchFor('string')
+  //   const stringNodes = searchFor('string')
 
-    console.log('STRINGS', stringNodes)
+  //   console.log('STRINGS', stringNodes)
 
-    const strings: HardStringInfo[] = stringNodes.map((i: any) => ({
-      range: new Range(
-        doc.positionAt(i.loc.start.offset),
-        doc.positionAt(i.loc.end.offset),
-      ),
-      value: i.value,
-    }))
+  //   const strings: HardStringInfo[] = stringNodes.map((i: any) => ({
+  //     range: new Range(
+  //       doc.positionAt(i.loc.start.offset),
+  //       doc.positionAt(i.loc.end.offset),
+  //     ),
+  //     value: i.value,
+  //   }))
 
-    return strings
-  }
+  //   return strings
+  // }
 }
 
 export default LaravelFramework
