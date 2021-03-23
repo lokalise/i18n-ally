e077a4b2591a85802db9a66ddbd8aa053261604d
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-23 19:11:51 +0800

    refactor: make "Current File" tab a tree with multiple entries, close #506

--- examples/by-features/demo/Demo.vue
+++ examples/by-features/demo/Demo.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <p>{{ $t('intro') }}</p>
-    <p>{{ $t('hello') }}</p> 
+    <p>{{ $t('hello') }}</p>
+    <p>{{ $t('not_exist') }}</p>
   </div>
 </template>
 
 <script>
 export default {
   computed: {
-    foobar () {
+    foobar() {
       return this.$t('good_morning')
     },
   },
