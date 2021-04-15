b7db3934f7b438dc600ac36c5bf0da087b272347
Jesper Bratt <jesper.bratt@challengermode.com>
2021-04-15 11:31:23 +0200

    fix: utilize Google API with new api endpoint

--- src/translators/engines/google.ts
+++ src/translators/engines/google.ts
@@ -5,6 +5,7 @@ import { Config } from '~/core'
 export default class GoogleTranslate extends TranslateEngine {
   link = 'https://translate.google.com'
   apiRoot = 'https://translate.googleapis.com'
+  apiRootIfUserSuppliedKey = 'https://translation.googleapis.com'
 
   async translate(options: TranslateOptions) {
     const {
@@ -13,20 +14,18 @@ export default class GoogleTranslate extends TranslateEngine {
     } = options
 
     const key = Config.googleApiKey
-    const headers = key
-      ? { Authorization: `Bearer ${key}` }
-      : undefined
 
     const { data } = await axios({
       method: 'GET',
-      url: `${this.apiRoot}/translate_a/single?client=gtx&sl=${from}&tl=${to}&hl=zh-CN&dt=t&dt=bd&ie=UTF-8&oe=UTF-8&dj=1&source=icon&q=${encodeURI(options.text)}`,
-      headers,
+      url: key
+        ? `${this.apiRootIfUserSuppliedKey}/language/translate/v2?key=${key}&q=${encodeURI(options.text)}&source=${from}&target=${to}&alt=json`
+        : `${this.apiRoot}/translate_a/single?client=gtx&sl=${from}&tl=${to}&hl=zh-CN&dt=t&dt=bd&ie=UTF-8&oe=UTF-8&dj=1&source=icon&q=${encodeURI(options.text)}`,
     })
 
-    return this.transform(data, options)
+    return this.transform(data, options, !!key)
   }
 
-  transform(response: any, options: TranslateOptions): TranslateResult {
+  transform(response: any, options: TranslateOptions, apiKeySuppliedByUser: boolean): TranslateResult {
     const {
       text,
       to = 'auto',
@@ -40,25 +39,38 @@ export default class GoogleTranslate extends TranslateEngine {
       linkToResult: `${this.link}/#auto/${to}/${text}`,
     }
 
-    // 尝试获取详细释义
-    try {
-      const d: string[] = []
-      response.dict.forEach((v: any) => {
-        d.push(`${v.pos}：${(v.terms.slice(0, 3) || []).join(',')}`)
-      })
-      r.detailed = d
+    if (apiKeySuppliedByUser) {
+      try {
+        const result: string[] = []
+        response.data.translations.forEach((v: any) => {
+          result.push(v.translatedText)
+        })
+        r.result = result
+      }
+      catch (e) {}
     }
-    catch (e) {}
 
-    // 尝试取得翻译结果
-    try {
-      const result: string[] = []
-      response.sentences.forEach((v: any) => {
-        result.push(v.trans)
-      })
-      r.result = result
+    else {
+      // 尝试获取详细释义
+      try {
+        const detailed: string[] = []
+        response.dict.forEach((v: any) => {
+          detailed.push(`${v.pos}：${(v.terms.slice(0, 3) || []).join(',')}`)
+        })
+        r.detailed = detailed
+      }
+      catch (e) {}
+
+      // 尝试取得翻译结果
+      try {
+        const result: string[] = []
+        response.sentences.forEach((v: any) => {
+          result.push(v.trans)
+        })
+        r.result = result
+      }
+      catch (e) {}
     }
-    catch (e) {}
 
     if (!r.detailed && !r.result)
       r.error = new Error('No result')
