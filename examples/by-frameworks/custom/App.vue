8e5196bca5d845ae14c154d5c9af7ad7c5b32b40
Anthony Fu <anthonyfu117@hotmail.com>
2021-06-08 20:31:50 +0800

    docs: add custom framework example

---
+++ examples/by-frameworks/custom/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>{{ customT('project') }}</p>
+</template>
+
+<script>
+export default {
+  computed: {
+    foobar() {
+      return customT('nested.foo.bar')
+    },
+  },
+}
+</script>
