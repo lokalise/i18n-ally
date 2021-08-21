43fb4997a743cce75f120c7228c7bd66a2ec55c9
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-08-21 21:12:33 +0300

    Implement writing SFC files

--- src/core/loaders/FluentVueSfcLoader.ts
+++ src/core/loaders/FluentVueSfcLoader.ts
@@ -1,11 +1,11 @@
-import { workspace, Uri } from 'vscode'
-import { parse as vueParse } from '@vue/compiler-sfc'
+import { workspace, Uri, WorkspaceEdit, Range } from 'vscode'
+import { parse as vueParse, SFCDescriptor, SFCBlock } from '@vue/compiler-sfc'
 import { PendingWrite, NodeOptions } from '../types'
 import { FluentParser } from '../../parsers/ftl'
 import { LocaleTree } from '../Nodes'
 import { Config } from '../Config'
 import { Loader } from './Loader'
-import { Log } from '~/utils'
+import { File, Log } from '~/utils'
 
 interface SFCFileInfo {
   path: string
@@ -103,10 +103,103 @@ export class FluentVueSfcLoader extends Loader {
 
     pendings = pendings.filter(i => i)
 
-    await this.load()
+    for (const pending of pendings) {
+      const path = pending.textFromPath
+      if (!path)
+        continue
+
+      const source = await File.read(path)
+      const parseResult = vueParse(source, { filename: pending.textFromPath })
+
+      let fluentBlock = parseResult.descriptor.customBlocks
+        .find(block => block.type === 'fluent' && block.attrs.locale === pending.locale)
+
+      const newTranslation = { [pending.keypath]: pending.value! }
+
+      if (fluentBlock) {
+        const newBlockContent = parser.merge(fluentBlock.content, newTranslation)
+        fluentBlock.content = newBlockContent
+      }
+      else {
+        fluentBlock = {
+          type: 'fluent',
+          attrs: {
+            locale: pending.locale,
+          },
+          content: parser.merge('', newTranslation),
+          loc: null!,
+        }
+      }
+
+      // Write back
+      const blocks = getBlocks(parseResult.descriptor)
+      const newContent = buildContent(fluentBlock, source, blocks)
+
+      const doc = await workspace.openTextDocument(this.uri)
+
+      if (doc.isDirty) {
+        const edit = new WorkspaceEdit()
+        edit.replace(this.uri, new Range(doc.positionAt(0), doc.positionAt(Infinity)), newContent)
+
+        await workspace.applyEdit(edit)
+      }
+      else {
+        await File.write(this.filepath, newContent)
+      }
+
+      await this.load()
+    }
   }
 
   canHandleWrites(pending: PendingWrite) {
     return pending.filepath === this.filepath
   }
+}
+
+function getBlocks(descriptor: SFCDescriptor): SFCBlock[] {
+  const { template, script, styles, customBlocks } = descriptor
+  const blocks: SFCBlock[] = [...styles, ...customBlocks]
+  template && blocks.push(template as SFCBlock)
+  script && blocks.push(script as SFCBlock)
+  blocks.sort((a, b) => {
+    return a.loc.start.offset - b.loc.start.offset
+  })
+  return blocks
+}
+
+function buildContent(blockToAdd: SFCBlock, raw: string, blocks: SFCBlock[]): string {
+  let offset = 0
+  let inserted = false
+  let contents: string[] = []
+  let fluentOffset = -1
+
+  contents = blocks.reduce((contents, block) => {
+    if (block.type === 'fluent' && block.attrs.locale === blockToAdd.attrs.locale) {
+      contents = contents.concat(raw.slice(offset, block.loc.start.offset))
+      contents = contents.concat(`\n${blockToAdd.content}`)
+      offset = block.loc.end.offset
+      inserted = true
+    }
+    else {
+      contents = contents.concat(raw.slice(offset, block.loc.end.offset))
+      offset = block.loc.end.offset
+
+      if (block.type === 'fluent')
+        fluentOffset = contents.length
+    }
+    return contents
+  }, contents)
+
+  contents = contents.concat(raw.slice(offset, raw.length))
+
+  if (!inserted) {
+    const content = `<fluent locale=${blockToAdd.attrs.locale}>\n${blockToAdd.content}</fluent>`
+
+    if (fluentOffset !== -1)
+      contents.splice(fluentOffset, 0, content)
+    else
+      contents.push(content)
+  }
+
+  return contents.join('')
 }
