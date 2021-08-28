59081cc72a2f04cfd7830b10031025940db9dcca
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-08-28 22:09:53 +0300

    Extract logic that manages locale messages

--- src/core/loaders/FluentVueSfcLoader.ts
+++ src/core/loaders/FluentVueSfcLoader.ts
@@ -1,47 +1,10 @@
 import { workspace, Uri, WorkspaceEdit, Range } from 'vscode'
-import { parse as vueParse } from '@vue/component-compiler-utils'
-import { VueTemplateCompiler } from '@vue/component-compiler-utils/dist/types'
-import { SFCDescriptor, SFCBlock } from '@vue/component-compiler-utils/dist/parse'
-import * as compiler from 'vue-template-compiler'
+import { getVueMessages, mergeVue, MessagesWithLocale } from 'fluent-vue-cli'
 import { PendingWrite, NodeOptions } from '../types'
-import { FluentParser } from '../../parsers/ftl'
 import { LocaleTree } from '../Nodes'
 import { Config } from '../Config'
 import { Loader } from './Loader'
 import { File, Log } from '~/utils'
-
-interface SFCFileInfo {
-  path: string
-  content: string
-}
-
-type SFCFluentBlock = {
-  locale: string
-  messages: Record<string, string>
-}
-
-const parser = new FluentParser()
-
-async function getFluentBlocks(file: SFCFileInfo): Promise<SFCFluentBlock[]> {
-  const descriptor = vueParse({
-    source: file.content,
-    filename: file.path,
-    compiler: compiler as VueTemplateCompiler,
-  })
-
-  const promises = descriptor.customBlocks.map(async(block) => {
-    if (block.type === 'fluent') {
-      return {
-        locale: block.attrs.locale,
-        messages: await parser.parse(block.content),
-      } as SFCFluentBlock
-    }
-
-    return null
-  }).filter(Boolean) as Promise<SFCFluentBlock>[]
-
-  return await Promise.all(promises)
-}
 
 export class FluentVueSfcLoader extends Loader {
   constructor(
@@ -64,9 +27,9 @@ export class FluentVueSfcLoader extends Loader {
     const filepath = this.filepath
     Log.info(`📑 Loading fluent-vue sfc ${filepath}`)
     const doc = await workspace.openTextDocument(this.uri)
-    const meta = await getFluentBlocks({ path: this.filepath, content: doc.getText() })
+    const blocks = getVueMessages(doc.getText())
 
-    this.updateLocalesTree(meta)
+    this.updateLocalesTree(blocks)
     this._onDidChange.fire(this.name)
   }
 
@@ -82,7 +45,7 @@ export class FluentVueSfcLoader extends Loader {
 
   _locales = new Set<string>()
 
-  private updateLocalesTree(blocks: SFCFluentBlock[]) {
+  private updateLocalesTree(blocks: MessagesWithLocale[]) {
     this._flattenLocaleTree = {}
     this._locales = new Set()
 
@@ -106,6 +69,8 @@ export class FluentVueSfcLoader extends Loader {
     if (!Array.isArray(pendings))
       pendings = [pendings]
 
+    Log.warn(JSON.stringify(pendings))
+
     pendings = pendings.filter(i => i)
 
     for (const pending of pendings) {
@@ -114,38 +79,13 @@ export class FluentVueSfcLoader extends Loader {
         continue
 
       const doc = await workspace.openTextDocument(this.uri)
-      const source = doc.getText()
-
-      const parseResult = vueParse({
-        source,
-        filename: pending.filepath ?? pending.textFromPath,
-        compiler: compiler as VueTemplateCompiler,
-      })
-
-      let fluentBlock = parseResult.customBlocks
-        .find(block => block.type === 'fluent' && block.attrs.locale === pending.locale)
+      const content = doc.getText()
 
       const newTranslation = { [pending.keypath]: pending.value! }
 
-      if (fluentBlock) {
-        const newBlockContent = parser.merge(fluentBlock.content, newTranslation)
-        fluentBlock.content = newBlockContent
-      }
-      else {
-        fluentBlock = {
-          type: 'fluent',
-          attrs: {
-            locale: pending.locale,
-          },
-          content: parser.merge('', newTranslation),
-          start: 0,
-          end: 0,
-        }
-      }
+      const newContent = mergeVue(content, pending.locale, newTranslation)
 
-      // Write back
-      const blocks = getBlocks(parseResult)
-      const newContent = buildContent(fluentBlock, source, blocks)
+      Log.warn(newContent)
 
       if (doc.isDirty) {
         const edit = new WorkspaceEdit()
@@ -164,52 +104,4 @@ export class FluentVueSfcLoader extends Loader {
   canHandleWrites(pending: PendingWrite) {
     return pending.filepath === this.filepath || pending.textFromPath === this.filepath
   }
-}
-
-function getBlocks(descriptor: SFCDescriptor): SFCBlock[] {
-  const { template, script, styles, customBlocks } = descriptor
-  const blocks: SFCBlock[] = [...styles, ...customBlocks]
-  template && blocks.push(template as SFCBlock)
-  script && blocks.push(script as SFCBlock)
-  blocks.sort((a, b) => {
-    return a.start! - b.start!
-  })
-  return blocks
-}
-
-function buildContent(blockToAdd: SFCBlock, raw: string, blocks: SFCBlock[]): string {
-  let offset = 0
-  let inserted = false
-  let contents: string[] = []
-  let fluentOffset = -1
-
-  contents = blocks.reduce((contents, block) => {
-    if (block.type === 'fluent' && block.attrs.locale === blockToAdd.attrs.locale) {
-      contents = contents.concat(raw.slice(offset, block.start))
-      contents = contents.concat(`\n${blockToAdd.content}`)
-      offset = block.end as number
-      inserted = true
-    }
-    else {
-      contents = contents.concat(raw.slice(offset, block.end))
-      offset = block.end as number
-
-      if (block.type === 'fluent')
-        fluentOffset = contents.length
-    }
-    return contents
-  }, contents)
-
-  contents = contents.concat(raw.slice(offset, raw.length))
-
-  if (!inserted) {
-    const content = `\n<fluent locale="${blockToAdd.attrs.locale}">\n${blockToAdd.content}</fluent>\n`
-
-    if (fluentOffset !== -1)
-      contents.splice(fluentOffset, 0, content)
-    else
-      contents.push(content)
-  }
-
-  return contents.join('')
 }
--- src/parsers/ftl.ts
+++ src/parsers/ftl.ts
@@ -1,68 +1,25 @@
-import { parse, serialize, FluentSerializer, Message, Pattern, Identifier, Entry } from '@fluent/syntax'
+import { mergeFtl, getFtlMessages } from 'fluent-vue-cli'
 import { Parser } from './base'
 import { File } from '~/utils'
 
 export class FluentParser extends Parser {
   id = 'ftl'
-  serializer: FluentSerializer
 
   constructor() {
     super(['ftl'], 'ftl')
-
-    this.serializer = new FluentSerializer()
-  }
-
-  getText(entry: Message): string {
-    entry.comment = null
-    return this.serializer.serializeEntry(entry).substring(entry.id.name.length + 3)
   }
 
   async parse(text: string) {
-    const data = parse(text, { withSpans: false }).body
-      .map((entry: Entry) => entry instanceof Message
-        ? [
-          entry.id.name,
-          this.getText(entry),
-        ]
-        : undefined)
-      .filter(Boolean) as string[][]
-
-    return Object.fromEntries(data)
+    return getFtlMessages(text)
   }
 
   async dump(): Promise<string> {
     throw new Error('Not implemented')
   }
 
-  merge(ftlSource: string, object: Record<string, string>): string {
-    const currentResourse = parse(ftlSource, { withSpans: true })
-
-    for (const [key, value] of Object.entries(object)) {
-      const resourceString = `${key} = ${value}`
-      const resource = parse(resourceString, { withSpans: false })
-
-      let updated = false
-      for (const message of currentResourse.body) {
-        if (message instanceof Message && message.id.name === key) {
-          updated = true
-          message.value = resource.body[0].value as Pattern
-        }
-      }
-
-      if (!updated) {
-        const newMessage = new Message(
-          new Identifier(key),
-          resource.body[0].value as Pattern)
-        currentResourse.body.push(newMessage)
-      }
-    }
-
-    return serialize(currentResourse, { withJunk: true })
-  }
-
   async save(filepath: string, object: Record<string, string>) {
     const currentFile = await File.read(filepath)
-    const text = this.merge(currentFile, object)
+    const text = mergeFtl(currentFile, object)
 
     await File.write(filepath, text)
   }
