b39150ba5e623192163ff75529b10a557e6e2c6e
Alex Terehov <terales@users.noreply.github.com>
2021-09-12 21:11:30 +0300

    fix: speed up NonAsciiExtractionRule

--- src/extraction/rules/non-ascii-characters.ts
+++ src/extraction/rules/non-ascii-characters.ts
@@ -6,9 +6,9 @@ export class NonAsciiExtractionRule extends ExtractionRule {
   shouldExtract(str: string) {
     // [^\u{0}-\u{7F}] -- non Latin script,see https://unicode.org/reports/tr18/#General_Category_Property
     const words = str.match(/\p{Letter}*/ug) ?? []
-    const wordsWithNonAsciiChars = words.filter(word => word.match(/[^\u{0}-\u{7F}]/u))
+    const containsWordWithNonAsciiChar = words.some(word => word.match(/[^\u{0}-\u{7F}]/u))
 
-    if (wordsWithNonAsciiChars.length > 0)
+    if (containsWordWithNonAsciiChar)
       return ExtractionScore.MustInclude
   }
 }
