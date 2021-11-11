b6a0c6b821ee9e7bec9d0316ed1db3277bf7b206
Robert Sasak <robert.sasak@gmail.com>
2021-11-11 10:02:22 +0100

    Support for i18n.js framework

---
+++ src/frameworks/i18n-js.ts
@@ -0,0 +1,40 @@
+import { TextDocument } from 'vscode'
+import { Framework, ScopeRange } from './base'
+import { RewriteKeySource, RewriteKeyContext } from '~/core'
+import { LanguageId } from '~/utils'
+
+class I18js extends Framework {
+  id ='i18n-js'
+  display = 'i18n.js'
+  namespaceDelimiter = '.'
+
+  detection = {
+    packageJSON: {
+      any: [
+        'i18n-js',
+      ],
+    },
+  }
+
+  languageIds: LanguageId[] = [
+    'javascript',
+    'typescript',
+    'javascriptreact',
+    'typescriptreact',
+    'ejs',
+  ]
+
+  // for visualize the regex, you can use https://regexper.com/
+  usageMatchRegex = [
+    '(?:i18n|I18n)\.t\(\s*[\'"`]({key})[\'"`]',
+  ]
+
+  refactorTemplates(keypath: string) {
+    return [
+      keypath,
+    ]
+  }
+
+}
+
+export default I18js
