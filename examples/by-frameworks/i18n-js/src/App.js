b8ae31ce4d78ee56dd4ba3b38b63b69d5cd36b86
Robert Sasak <robert.sasak@gmail.com>
2021-11-11 10:43:07 +0100

    i18n example

--- examples/by-frameworks/i18n-js/src/App.js
+++ examples/by-frameworks/i18n-js/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import i18n from './locales';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          {i18n.t('home.headline')}
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <b>./src/locales/index.js</b> to try different language.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
