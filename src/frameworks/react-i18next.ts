2a493e6a1ee998483958d96a19e071cf165aabd0
Simeon Cheeseman <simeon@thehavens.co.nz>
2022-03-01 17:47:46 +0900

    fix: correctly extend react-i18next from i18next framework

---
+++ src/frameworks/react-i18next.ts
@@ -0,0 +1,39 @@
+import { I18nextFramework } from './i18next'
+import { LanguageId } from '~/utils'
+
+class ReactI18nextFramework extends Framework {
+  id= 'react-i18next'
+  display= 'React'
+
+  detection= {
+    packageJSON: [
+      'react-i18next',
+      'next-i18next',
+    ],
+  }
+
+  languageIds: LanguageId[] = [
+    'javascript',
+    'typescript',
+    'javascriptreact',
+    'typescriptreact',
+    'ejs',
+  ]
+
+  // for visualize the regex, you can use https://regexper.com/
+  usageMatchRegex = [
+    // general jsx attrs
+    '[^\\w\\d](?:i18nKey=|FormattedMessage[ (]\\s*id=|t\\(\\s*)[\'"`]({key})[\'"`]',
+    '<Trans>({key})<\\/Trans>',
+  ]
+
+  refactorTemplates(keypath: string) {
+    return [
+      `{t('${keypath}')}`,
+      `t('${keypath}')`,
+      keypath,
+    ]
+  }
+}
+
+export default ReactI18nextFramework
