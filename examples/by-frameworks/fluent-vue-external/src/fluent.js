09b0c28962dc08ee62de42e7312494fe323d53ac
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-11-21 06:22:44 +0100

    Remove example

--- examples/by-frameworks/fluent-vue-external/src/fluent.js
+++
@@ -1,9 +0,0 @@
-import { FluentBundle } from '@fluent/bundle'
-import { createFluentVue } from 'fluent-vue'
-
-const bundle = new FluentBundle('en')
-
-export const fluent = createFluentVue({
-  locale: 'en',
-  bundles: [bundle],
-})
