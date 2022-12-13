99ea0547af22c98fab3c1246ba5af2446ba74ee2
Amanda Ranard <amanda.ranard@thetradedesk.com>
2022-12-13 12:28:17 -0700

    Feat: Adding new namespaceMatchRegex config to the custom framework options

---
+++ examples/by-features/custom-scope-range/src/index.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-use-before-define
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import './i18n'
+
+ReactDOM.render(<App />, document.getElementById('root'))
