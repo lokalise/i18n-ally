c530ed3da0fcff32a736f49a982bf306edaf7b6a
Jan Amann <jan@amann.work>
2023-05-22 11:43:41 +0200

    Basic PoC

--- examples/by-frameworks/next-intl/src/app/[locale]/page.tsx
+++ examples/by-frameworks/next-intl/src/app/[locale]/page.tsx
@@ -5,9 +5,26 @@ import { useTranslations } from 'next-intl'
 export default function IndexPage() {
   const t = useTranslations('IndexPage')
 
+  t('title')
+  t.rich('title')
+  t.raw('title')
+
   return (
     <div>
       <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
+      <Test1 />
+      <Test2 />
     </div>
   )
+}
+
+function Test1() {
+  const t = useTranslations('Test')
+  return <>{t('title')}</>
+}
+
+function Test2() {
+  const t = useTranslations()
+  return <>{t('Test.title')}</>
 }
--- src/frameworks/next-intl.ts
+++ src/frameworks/next-intl.ts
@@ -25,128 +25,81 @@ class NextIntlFramework extends Framework {
     'ejs',
   ]
 
-  // for visualize the regex, you can use https://regexper.com/
   usageMatchRegex = [
-    '\\Wt\\({key})',
-    // - "[^\\w\\d]t\\(['\"`]({key})['\"`]"
+    // Basic usage
+    '[^\\w\\d]t\\([\'"`]({key})[\'"`]',
 
-    // todo: rich / raw / values
-    // '\\Wt(\.(rich|raw)?)?\\(\\s*[\'"`]({key})[\'"`]',
+    // Rich text
+    '[^\\w\\d]t\.rich\\([\'"`]({key})[\'"`]',
+
+    // Raw text
+    '[^\\w\\d]t\.raw\\([\'"`]({key})[\'"`]',
   ]
 
-  // derivedKeyRules = [
-  //   '{key}_plural',
-  //   '{key}_0',
-  //   '{key}_1',
-  //   '{key}_2',
-  //   '{key}_3',
-  //   '{key}_4',
-  //   '{key}_5',
-  //   '{key}_6',
-  //   '{key}_7',
-  //   '{key}_8',
-  //   '{key}_9',
-  //   // support v4 format as well as v3
-  //   '{key}_zero',
-  //   '{key}_one',
-  //   '{key}_two',
-  //   '{key}_few',
-  //   '{key}_many',
-  //   '{key}_other',
-  // ]
-
   refactorTemplates(keypath: string) {
+    // Provide options where the last segment is the keypath.
+    // Ideally we'd automatically consider the namespace here.
+    const lastSegment = keypath.split('.').pop()
+
     return [
       `{t('${keypath}')}`,
+      `{t('${lastSegment}')}`,
       `t('${keypath}')`,
+      `t('${lastSegment}')`,
     ]
   }
 
-  // rewriteKeys(key: string, source: RewriteKeySource, context: RewriteKeyContext = {}) {
-  //   const dottedKey = key.split(this.namespaceDelimitersRegex).join('.')
+  rewriteKeys(key: string, source: RewriteKeySource, context: RewriteKeyContext = {}) {
+    const dottedKey = key.split(this.namespaceDelimitersRegex).join('.')
 
-  //   // when explicitly set the namespace, ignore current namespace scope
-  //   if (
-  //     this.namespaceDelimiters.some(d => key.includes(d))
-  //     && context.namespace
-  //     && dottedKey.startsWith(context.namespace.split(this.namespaceDelimitersRegex).join('.'))
-  //   )
-  //     // +1 for the an extra `.`
-  //     key = key.slice(context.namespace.length + 1)
+    // When the namespace is explicitly set, ignore the current namespace scope
+    if (
+      this.namespaceDelimiters.some(delimiter => key.includes(delimiter))
+      && context.namespace
+      && dottedKey.startsWith(context.namespace.split(this.namespaceDelimitersRegex).join('.'))
+    ) {
+      // +1 for the an extra `.`
+      key = key.slice(context.namespace.length + 1)
+    }
 
-  //   // replace colons
-  //   return dottedKey
-  // }
+    return dottedKey
+  }
 
-  // useTranslation
-  // https://react.i18next.com/latest/usetranslation-hook#loading-namespaces
-  // getScopeRange(document: TextDocument): ScopeRange[] | undefined {
-  //   if (!this.languageIds.includes(document.languageId as any))
-  //     return
+  getScopeRange(document: TextDocument): ScopeRange[] | undefined {
+    if (!this.languageIds.includes(document.languageId as any))
+      return
 
-  //   const ranges: ScopeRange[] = []
-  //   const text = document.getText()
+    const ranges: ScopeRange[] = []
+    const text = document.getText()
 
-  //   // Add smaller local scope overrides first
-  //   // Namespaced prefixed keys already handled by rewriteKeys
+    // Find matches of `useTranslations`, later occurences will override
+    // previous ones (this allows for multiple components with different
+    // namespaces in the same file).
+    const regex = /useTranslations\(\s*(['"`](.*?)['"`])?/g
+    let prevGlobalScope = false
+    for (const match of text.matchAll(regex)) {
+      if (typeof match.index !== 'number')
+        continue
 
-  //   // t('foo', { ns: 'ns1' })
-  //   const regT = /\Wt\([^)]*?ns:\s*['"`](\w+)['"`]/g
+      const namespace = match[2]
 
-  //   for (const match of text.matchAll(regT)) {
-  //     if (typeof match.index !== 'number')
-  //       continue
+      // End previous scope
+      if (prevGlobalScope)
+        ranges[ranges.length - 1].end = match.index
 
-  //     if (match[1]) {
-  //       ranges.push({
-  //         start: match.index,
-  //         end: match.index + match[0].length,
-  //         namespace: match[1],
-  //       })
-  //     }
-  //   }
+      // Start a new scope if a namespace is provided
+      if (namespace) {
+        prevGlobalScope = true
+        ranges.push({
+          start: match.index,
+          end: text.length,
+          namespace,
+        })
+      }
+    }
 
-  //   // <Trans i18nKey="foo" ns="ns1" />
-  //   const regTrans = /\Wi18nKey=(?:(?!\/Trans>|\/>)[\S\s])*?ns=\s*['"`](.+?)['"`]/g
-
-  //   for (const match of text.matchAll(regTrans)) {
-  //     if (typeof match.index !== 'number')
-  //       continue
-
-  //     if (match[1]) {
-  //       ranges.push({
-  //         start: match.index,
-  //         end: match.index + match[0].length,
-  //         namespace: match[1],
-  //       })
-  //     }
-  //   }
-
-  //   // Add first namespace as a global scope resetting on each occurrence
-  //   // useTranslation(ns1) and useTranslation(['ns1', ...])
-  //   const regUse = /useTranslation\(\s*\[?\s*['"`](.*?)['"`]/g
-  //   let prevGlobalScope = false
-  //   for (const match of text.matchAll(regUse)) {
-  //     if (typeof match.index !== 'number')
-  //       continue
-
-  //     // end previous scope
-  //     if (prevGlobalScope)
-  //       ranges[ranges.length - 1].end = match.index
-
-  //     // start a new scope if namespace is provided
-  //     if (match[1]) {
-  //       prevGlobalScope = true
-  //       ranges.push({
-  //         start: match.index,
-  //         end: text.length,
-  //         namespace: match[1],
-  //       })
-  //     }
-  //   }
-
-  //   return ranges
-  // }
+    return ranges
+  }
 }
 
 export default NextIntlFramework
