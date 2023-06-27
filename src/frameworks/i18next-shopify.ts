a46ee2152d24ad9ed1ad316c14cc9f1c1aa752a4
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2023-06-27 15:23:24 -0400

    Fixes usage report for non-missing derived keys
    Derived keys (e.g., such as plurals) were being miscategorised as
    missing in the usage report.  This commit checks if keys derived from
    those found in the missing list (keys that are in use but considered
    "not defined") are included in the list of idle keys (keys defined but
    not considered in use), indicating that they are indeed defined and in
    use.
    
    Fixes https://github.com/lokalise/i18n-ally/issues/953

--- src/frameworks/i18next-shopify.ts
+++ src/frameworks/i18next-shopify.ts
@@ -13,6 +13,27 @@ class ShopifyI18nextFramework extends ReactI18nextFramework {
       '@shopify/i18next-shopify',
     ],
   }
+
+  derivedKeyRules = [
+    '{key}.plural',
+    '{key}.0',
+    '{key}.1',
+    '{key}.2',
+    '{key}.3',
+    '{key}.4',
+    '{key}.5',
+    '{key}.6',
+    '{key}.7',
+    '{key}.8',
+    '{key}.9',
+    // support v4 format as well as v3
+    '{key}.zero',
+    '{key}.one',
+    '{key}.two',
+    '{key}.few',
+    '{key}.many',
+    '{key}.other',
+  ]
 }
 
 export default ShopifyI18nextFramework
