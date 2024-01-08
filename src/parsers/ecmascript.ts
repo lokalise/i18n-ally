9bb89cb7ce0eb829f9eeada681049d769f53e237
Anton Vlasov <nanostet@gmail.com>
2024-01-08 21:21:57 +0200

    add write to typescript files

--- src/parsers/ecmascript.ts
+++ src/parsers/ecmascript.ts
@@ -1,9 +1,10 @@
 import child_process from 'child_process'
 import path from 'path'
 import { Parser } from './base'
-import i18n from '~/i18n'
-import { Log } from '~/utils'
 import { Config, Global } from '~/core'
+import { getFramework } from '~/frameworks'
+import CustomFramework from '~/frameworks/custom'
+import { File, Log } from '~/utils'
 
 const LanguageIds = {
   js: 'javascript',
@@ -16,18 +17,48 @@ const LanguageExts = {
 } as const
 
 export class EcmascriptParser extends Parser {
-  readonly readonly = true
+  readonly readonly = false
+
+  private framework: CustomFramework
 
   constructor(public readonly id: 'js'|'ts' = 'js') {
     super([LanguageIds[id]], LanguageExts[id])
+    this.framework = getFramework('custom') as CustomFramework
   }
 
   async parse() {
     return {}
   }
 
-  async dump() {
-    return ''
+  async dump(object: object) {
+    return JSON.stringify(object, null, 2)
+  }
+
+  async save(filepath: string, object: object): Promise<void> {
+    const templates = this.framework.tsFileTemplates
+
+    let selectedTemplate: string | undefined
+    for (const [key, template] of Object.entries(templates)) {
+      if (filepath.endsWith(key)) {
+        selectedTemplate = template
+        break
+      }
+    }
+
+    if (!selectedTemplate) {
+      if (templates.default)
+        selectedTemplate = templates.default
+    }
+
+    if (!selectedTemplate) {
+      Log.error(`[i18n-ally] no template found for ${filepath}`)
+      return
+    }
+
+    const localization = await this.dump(object)
+    const text = selectedTemplate.replace('$1', localization)
+
+    await File.write(filepath, text)
   }
 
   async load(filepath: string) {
@@ -57,9 +88,5 @@ export class EcmascriptParser extends Parser {
         }
       })
     })
-  }
-
-  async save() {
-    Log.error(i18n.t('prompt.writing_js'))
   }
 }
