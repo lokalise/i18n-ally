2cb652864efac8dc61117f920e3d9beff0ab1884
Michael Noser <michael@noser.moe>
2024-02-26 18:33:16 +0100

    Support for multiline strings & find in file

--- src/parsers/po.ts
+++ src/parsers/po.ts
@@ -1,6 +1,8 @@
+/* eslint-disable import/order */
 import PO from 'pofile'
-import { Parser } from './base'
+import { KeyStyle } from '~/core'
 import { File } from '~/utils'
+import { Parser } from './base'
 
 export class PoParser extends Parser {
   id = 'po'
@@ -14,11 +16,12 @@ export class PoParser extends Parser {
     const result: Record<string, string> = {}
     for (const item of items) {
       if (item.msgstr.length)
-        result[item.msgid] = item.msgstr[0]
+        result[item.msgid] = item.msgstr.join()
     }
     return result
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async dump(object: object) {
     return ''
   }
@@ -29,9 +32,15 @@ export class PoParser extends Parser {
     const po_items = PO.parse(raw)
 
     const items = Object.keys(object).map((key) => {
+      const current = po_items.items.find(x => x.msgid === key)
       const item = new PO.Item()
       item.msgid = key
-      item.msgstr = [object[key]]
+      item.msgstr = [object[key].replace(/\n\n/g, '\n')]
+      item.comments = current?.comments || []
+      item.extractedComments = current?.extractedComments || []
+      item.msgctxt = current?.msgctxt
+      item.references = current?.references || []
+      item.flags = current?.flags || {}
       return item
     })
 
@@ -49,5 +58,15 @@ export class PoParser extends Parser {
           resolve()
       })
     })
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  override navigateToKey(text: string, keypath: string, keystyle: KeyStyle) {
+    const index = text.search(`"${keypath}"`)
+    if (index === -1) return undefined
+
+    const start = index + 1
+    const end = start + keypath.length
+    return { start, end, key: keypath, quoted: true }
   }
 }
